c92d99f2e2a40e3981907be3ced9cd3251f96290
Tadii3108 <vanessazingoni@gmail.com>
2020-06-24 08:52:46 +0200

    initial commit - html and css (green)

---
+++ script.js
@@ -0,0 +1,11 @@
+TweenMax.to(".loading-screen", 4, {
+    delay: 6.6,
+    top: "-110%",
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".logo", 3, {
+    delay: 6.6,
+    top: "-110%",
+    ease: Expo.easeInOut
+});
